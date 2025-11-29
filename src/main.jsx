eb4b8bca554d0e5183c504c42134b6627c29fbe8
OctavianCristian22 <octaviancristianedutanu@gmail.com>
2025-11-29 23:14:37 +0200

    a

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppWrapper from '@/AppWrapper';
-import { Toaster } from '@/components/ui/toaster';
+import { Toaster } from '@/components/ui/Toaster';
 import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
